8d8276242b8260d75007e01f2b09d02fbdec6248
Magnus Bengtsson <mbengtsson@redbridge.se>
2014-12-26 00:10:22 +0100

    Initial rbc adaptaions

--- FirstLogon.ps1
+++ FirstLogon.ps1
@@ -86,7 +86,7 @@ $logonScriptPath = "$ENV:SystemRoot\Temp\Logon.ps1"
 try
 {
     $Host.UI.RawUI.WindowTitle = "Downloading Logon script..."
-    $baseUrl = "https://raw.github.com/cloudbase/windows-openstack-imaging-tools/master"
+    $baseUrl = "https://raw.github.com/redbridge/windows-openstack-imaging-tools/master"
     (new-object System.Net.WebClient).DownloadFile("$baseUrl/Logon.ps1", $logonScriptPath)
 
     $hypervisorStr = getHypervisor
